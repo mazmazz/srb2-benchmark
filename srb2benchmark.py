7997ab85e7ac8f2fab26861a59a010f80a3d4260
mazmazz <mar.marcoz@outlook.com>
2018-12-29 05:45:24 -0500

    Remove default list hack for Gooey

--- srb2benchmark.py
+++ srb2benchmark.py
@@ -133,36 +133,24 @@ def get_args():
     parser.add_argument('-cwd', type=str, default='..',
         help='Current working directory to start SRB2 from (i.e., your root SRB2 folder)')
     parser.add_argument('-exe', type=str, nargs='+',
-        default=' '.join(map(str,
-            ['32-bit/srb2win-v2123.exe','64-bit/srb2win-v2123.exe']
-            )),
+        default=['32-bit/srb2win-v2123.exe','64-bit/srb2win-v2123.exe'],
         help='Space-separated list of EXE filenames to test.')
     parser.add_argument('-id', type=str, nargs='+',
-        default=' '.join(map(str,
-            ['vanilla','vanilla']
-            )),
+        default=['vanilla','vanilla'],
         help='Space-separated list of IDs to associate with the EXEs. # of IDs must match the # of EXEs.')
     parser.add_argument('-nosoftware', type=str2bool, nargs='+',
-        default=' '.join(map(str,
-            [False, False]
-            )),
+        default=[False, False],
         help='Do not benchmark Software mode. Space-separated true or false per EXE')
     parser.add_argument('-opengl', type=str2bool, nargs='+',
-        default=' '.join(map(str,
-            [False, False]
-            )),
+        default=[False, False],
         help='Benchmark OpenGL mode. Space-separated true or false per EXE')
     parser.add_argument('-vidmode', type=int, nargs='+',
-        default=' '.join(map(str,
-            [0, 15]
-            )),
+        default=[0, 15],
         help='Space-separated list of vid_modes to test, for all EXEs. 0 = 1920x1200; 9 = 1280x800; 15 = 640x400')
 
     parser.add_argument('-demo', type=str, nargs='+',
-        default=' '.join(map(str,
-            ['benchmark/replay/main/MAP01-guest.lmp',
-                'benchmark/replay/main/MAP02-guest.lmp']
-            )),
+        default=['benchmark/replay/main/MAP01-guest.lmp',
+                'benchmark/replay/main/MAP02-guest.lmp'],
         help='Space-separated list of demo filenames to benchmark, relative to the CWD.')
     parser.add_argument('-trials', type=int, default=3,
         help='Number of trials per test')
